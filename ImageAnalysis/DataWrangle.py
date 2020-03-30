4ebecf24392a2348dccbff0efa35ff88f9a95ca6
Eklyth <jeremie.doehla@gmail.com>
2020-03-29 20:26:35 -0700

    Add code for cleaning data to ensure conformity to image size.

--- ImageAnalysis/DataWrangle.py
+++ ImageAnalysis/DataWrangle.py
@@ -18,6 +18,59 @@ def fetch_contents(dir_path):
             yield x_path
 
 
+def transform_image_data(img_data):
+    dim_count = (
+        150,
+        150,
+        3  # channels
+    )
+    shape_data = img_data.shape
+
+    def extract_middle(idx):
+        x_lower =   int(np.floor((shape_data[idx] - dim_count[idx]) / 2))
+        x_upper = -1*int(np.ceil((shape_data[idx] - dim_count[idx]) / 2))
+        # TODO: It would be great to generalize this part too...
+        if idx == 0:
+            img_altered = img_data[x_lower:x_upper,:,:]
+        elif idx == 1:
+            img_altered = img_data[:,x_lower:x_upper,:]
+        ## end todo
+        return img_altered
+
+    def append_average(idx):
+        # add to the image using the average of the data in the associated row
+        img_altered = img_data
+        fill_values = np.divide(np.sum(img_data, axis=idx), shape_data[idx]).astype(int)
+        # TODO: It would be great to generalze this part too...
+        x_shape = fill_values.shape
+        if idx == 0:
+            x_shape = (1, x_shape[0], x_shape[1])
+        elif idx == 1:
+            x_shape = (x_shape[0], 1, x_shape[1])
+        ## end todo
+        fill_values = np.reshape(fill_values, x_shape)
+        for _ in range(dim_count[idx] - shape_data[idx]):
+            img_altered = np.append(img_altered, fill_values, axis=idx)
+        return img_altered
+
+    # If the shape of the image is not as expected, change it to that expected
+    if not (shape_data == dim_count):
+        if not (shape_data[2] == dim_count[2]):
+            # Not 3 channel. This is a larger issue.
+            img_data = None
+        else:
+            for i in range(2):
+                if shape_data[i] > dim_count[i]:
+                    img_data = extract_middle(i)
+                elif shape_data[i] < dim_count[i]:
+                    img_data = append_average(i)
+
+    # Image data has values from 0 to 255. Convert these to [0,1]
+    # /255
+
+    return img_data
+
+
 def construct_file_df(dir_path, include_labels=False):
     """Construct a dataframe with file_path
         and optionally the label of the file.
@@ -38,15 +91,11 @@ def construct_file_df(dir_path, include_labels=False):
             data_elem['file_label'] = label
 
         img = mpimg.imread(file)
-        data_elem['file_img'] = img
+        img = transform_image_data(img)
         data.append(data_elem)
 
     df = pd.DataFrame(data, columns=cols)
     return df
-
-
-
-
 
 
 if __name__ == '__main__':
@@ -56,6 +105,6 @@ if __name__ == '__main__':
     f_output = os.path.join(f_root, 'data', 'packaged', 'training_data.csv')
 
     df = construct_file_df(f_data_root, include_labels=True)
-    df.to_csv(f_output, index=False)
+    # df.to_csv(f_output, index=False)
 
     print(df.info())
